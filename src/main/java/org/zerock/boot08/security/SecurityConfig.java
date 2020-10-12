319d2e44e62e3c052db07f3b9370fc530d038368
kimminkwon <kmk130519@gmail.com>
2020-10-12 16:13:59 +0900

    Add the default settings for security
    201012

--- src/main/java/org/zerock/boot08/security/SecurityConfig.java
+++ src/main/java/org/zerock/boot08/security/SecurityConfig.java
@@ -1,13 +1,34 @@
 package org.zerock.boot08.security;
 
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Log
 @EnableWebSecurity // Bean으로 인식되도록 하는 것
 // WebSecurityConfigurerAdapter = 설정을 담당하는 클래스
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("security config.......................");
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return rawPassword.equals(encodedPassword);
+            }
+        };
+    }
 }
