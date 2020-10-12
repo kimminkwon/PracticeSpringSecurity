dd66cb4ea70166df2c32d58749e4cada0ee16fa0
kimminkwon <kmk130519@gmail.com>
2020-10-12 17:58:51 +0900

    make basic login function
    201012

--- src/main/java/org/zerock/boot08/security/SecurityConfig.java
+++ src/main/java/org/zerock/boot08/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package org.zerock.boot08.security;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +17,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         log.info("security config.......................");
+        http.authorizeRequests().antMatchers("/guest/**").permitAll();
+        http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
+        http.formLogin();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        log.info("build auth global.......................");
+        auth.inMemoryAuthentication()
+                .withUser("manager")
+                .password("1111")
+                .roles("MANAGER");
     }
 
     @Bean
