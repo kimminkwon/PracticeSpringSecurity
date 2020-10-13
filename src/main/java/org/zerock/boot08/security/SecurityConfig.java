f5813c5de6d7ff25c25aed247704b80455d2ddd0
kimminkwon <kmk130519@gmail.com>
2020-10-13 11:18:13 +0900

    make basic Authentication using jdbc
    201013

--- src/main/java/org/zerock/boot08/security/SecurityConfig.java
+++ src/main/java/org/zerock/boot08/security/SecurityConfig.java
@@ -9,10 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Log
 @EnableWebSecurity // Bean으로 인식되도록 하는 것
 // WebSecurityConfigurerAdapter = 설정을 담당하는 클래스
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,17 +26,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
         http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
 
+        // 로그인 페이지
         http.formLogin().loginPage("/login");
+        // 접근 거부되었을 때 이동할 페이지
         http.exceptionHandling().accessDeniedPage("/accessDenied");
+        // 세선 무효화
+        http.logout().logoutUrl("/logout").invalidateHttpSession(true);
     }
 
+    // 인증 매니저를 생성하는 메서드
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         log.info("build auth global.......................");
+
+        /*
         auth.inMemoryAuthentication()
                 .withUser("manager")
                 .password("1111")
                 .roles("MANAGER");
+         */
+
+        // Spring security 에서 사용하는 정보: username, password, enabled(해당 계정이 사용가능한가?)
+        String query1 = "SELECT uid username, upw password, true enabled FROM tbl_members WHERE uid= ?";
+        String query2 = "SELECT member uid, role_name role FROM tbl_member_roles WHERE member = ?";
+
+        auth.jdbcAuthentication().
+                dataSource(dataSource).
+                usersByUsernameQuery(query1).
+                rolePrefix("ROLE_")
+                .authoritiesByUsernameQuery(query2);
     }
 
     @Bean
